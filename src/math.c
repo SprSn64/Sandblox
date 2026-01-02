8147027060987b333d7e738506f44fbd1286ba51
SprSn64 <136403175+SprSn64@users.noreply.github.com>
2026-01-02 14:37:17 +1100

    more matrix functions

--- src/math.c
+++ src/math.c
@@ -22,13 +22,39 @@ float dotProd3(Vector3 vecA, Vector3 vecB){
 	return vecA.x * vecB.x + vecA.y * vecB.y + vecA.z * vecB.z;
 }
 
-Vector4 transMatrix(Vector4 vector, mat4 matrix){
+float *newMatrix(){
+	float *output;
+	output = malloc(sizeof(mat4));
+	output[0] = 1; output[5] = 1; output[10] = 1; output[15] = 1; 
+	return output;
+}
+
+Vector4 matrixMult(Vector4 vector, mat4 matrix){
 	return (Vector4){
 		vector.x * matrix[0] + vector.y * matrix[1] + vector.z * matrix[2] + vector.w * matrix[3], 
 		vector.x * matrix[4] + vector.y * matrix[5] + vector.z * matrix[6] + vector.w * matrix[7],  
 		vector.x * matrix[8] + vector.y * matrix[9] + vector.z * matrix[10] + vector.w * matrix[11], 
 		vector.x * matrix[12] + vector.y * matrix[13] + vector.z * matrix[14] + vector.w * matrix[15], 
 	};
+}
+
+/*matrix[0], matrix[1], matrix[2], matrix[3],
+matrix[4], matrix[5], matrix[6], matrix[7],
+matrix[8], matrix[9], matrix[10], matrix[11],
+matrix[12], matrix[13], matrix[24], matrix[15],*/
+
+float *scaleMatrix(mat4 matrix, Vector3 scale){
+	float *output;
+	output = malloc(sizeof(mat4));
+	float tempMatrix[16] = {
+		matrix[0] * scale.x, matrix[1], matrix[2], matrix[3],
+		matrix[4], matrix[5] * scale.y, matrix[6], matrix[7],
+		matrix[8], matrix[9], matrix[10] * scale.z, matrix[11],
+		matrix[12], matrix[13], matrix[24], matrix[15],
+	};
+	
+	memcpy(output, &tempMatrix, sizeof(mat4));
+	return output;
 }
 
 float *multMatrix(mat4 matrixA, mat4 matrixB){
@@ -53,5 +79,28 @@ float *multMatrix(mat4 matrixA, mat4 matrixB){
 	output[14] = matrixA[2] * matrixB[12] + matrixA[6] * matrixB[13] + matrixA[10] * matrixB[14] + matrixA[14] * matrixB[15];
 	output[15] = matrixA[3] * matrixB[12] + matrixA[7] * matrixB[13] + matrixA[11] * matrixB[14] + matrixA[15] * matrixB[15];
 
+	return output;
+}
+
+float *axisRotMatrix(Uint8 axis, float angle){ //axis 0 = x (yz planes), axis 1 = y (xz planes), axis 2 = z (xy planes)
+	float *output;
+	output = malloc(sizeof(mat4));
+	float angSin = SDL_sin(angle);
+	float angCos = SDL_cos(angle);
+	float tempMatrix[16] = {
+		angCos * (axis != 0) + (axis == 0), -angSin * (axis == 2), angSin * (axis == 1), 0,
+		angSin * (axis == 2), angCos * (axis != 1) + (axis == 1), -angSin * (axis == 0), 0,
+		-angSin * (axis == 1), angSin * (axis == 0), angCos * (axis != 2) + (axis == 2), 0,
+		0, 0, 0, 1,
+	};
+	memcpy(output, &tempMatrix, sizeof(mat4));
+	return output;
+}
+
+float *rotateMatrix(mat4 matrix, Vector3 angle){
+	float *output;
+	output = malloc(sizeof(mat4));
+	
+	//memcpy(output, &tempMatrix, sizeof(mat4));
 	return output;
 }
--- src/math.h
+++ src/math.h
@@ -6,7 +6,11 @@ float invLerp(float a, float b, float v);
 float dotProd2(SDL_FPoint vecA, SDL_FPoint vecB);
 float dotProd3(Vector3 vecA, Vector3 vecB);
 
-Vector4 transMatrix(Vector4 vector, mat4 matrix);
+Vector4 matrixMult(Vector4 vector, mat4 matrix);
+float *scaleMatrix(mat4 matrix, Vector3 scale);
+
 float *multMatrix(mat4 matrixA, mat4 matrixB);
+
+float *axisRotMatrix(Uint8 axis, float angle);
 
 #endif
